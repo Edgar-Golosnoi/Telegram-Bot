7428039b12d26d1aaae4c5bff98dad804e7b7256
Edgar <edgar.golosnoi@yandex.ru>
2022-12-05 00:54:05 +0300

    Wheather chat done

--- bot.js
+++ bot.js
@@ -1,19 +1,26 @@
 /* eslint-disable no-undef */
-const axios = require('axios');
 const { Telegraf } = require('telegraf');
+const axios = require('axios');
+
 
 const bot = new Telegraf('5930144928:AAGZgzG7VlnGeSqIRWrnMMfUeINx1aGm1cE');
 bot.start((ctx) => ctx.reply('Привет друг, я расскажу тебе о погоде, скинь свою геолокацию...'));
 bot.on('message', async (ctx) => {
-  console.log(ctx.message.location);
-  if(ctx.message.locacion){
-  console.log(ctx.message.location);
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=&{ctx.message.location.latitude}&lon=&{ctx.message.location.lolgitude}&appid={5930144928:AAGZgzG7VlnGeSqIRWrnMMfUeINx1aGm1cE}`
-  const response = await axios.get(url);c0053
-  console.log(response);
-  ctx.reply(`${response.data.name}: ${response.data.main.temp} C`);
-  }
-}),
+    if (ctx.message.location) {
+    console.log(ctx.message.location);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&appid=8ddfbcf8a3d045597a2848e80d7c0053`
+    const response = await axios.get(url);
+    console.log(response);
+    ctx.reply(`${response.data.name},
+    Температура: ${response.data.main.temp / 32 }°,
+    Скорость ветра в сукунду: ${response.data.wind.speed},
+    Ощущается как ${response.data.main.feels_like / 32}°,
+    Облачность: ${response.data.clouds.all} % ,
+    Высота над уровнем моря: ${response.data.main.sea_level} ,
+    Страна: ${response.data.sys.country} 
+    `)
+    }
+})
 bot.launch();
 
 // Enable graceful stop
